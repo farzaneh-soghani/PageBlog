38c78324d716c4ca17d7a36b903d87b4ab2deedd
Farzaneh Soghani <farzanehsoghani@gmail.com>
2026-09-20 22:05:02 +0200

    Konfiguration für automatische Erkennung von Lokal- und Produktionsumgebung hinzugefügt

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 function getConfig($key) {
+    $isLocal = ($_SERVER['HTTP_HOST'] ?? '') === 'localhost' || str_contains($_SERVER['HTTP_HOST'] ?? '', '127.0.0.1');
+    
     $config = [
         'title'     => 'PageBlog',
-        'dbhost'    => '127.0.0.1',
-        'db'        => 'pageblog', 
-        'dbuser'    => 'root',     
-        'dbpass'    => '',         
+        'dbhost'    => 'sqlXXX.infinityfree.com', 
+        'db'        => 'if0_42964925_pageblock_db', 
+        'dbuser'    => 'if0_42964925',            
+        'dbpass'    => '24BBImHqybswtKp',           
         'dbcharset' => 'utf8mb4'
     ];
     
